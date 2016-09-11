3510f7fb31b3d54665c5f75db34ebc10fc80f2ab
Mshary AlSharekh <sentient@Msharys-MBP.zain>
2016-09-11 17:37:28 +0300

    none

--- app/models.py
+++ app/models.py
@@ -73,7 +73,7 @@ class OfferedService(models.Model):
 	category = models.CharField(max_length=1337, default="other")
 
 	def __str__(self):
-		return "%s" % self.service.title
+		return "title"
 
 #============================= SUPPORT MODELS ==============================#
 
