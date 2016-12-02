cff16580d841536d0bcfcd0df9eca8f96821a1a1
sentient64 <m.alsharekh94@gmail.com>
2016-12-02 12:18:13 +0300

    None

--- app/views.py
+++ app/views.py
@@ -405,7 +405,7 @@ class PublicServiceView(APIView):
 				bidon = Bid.objects.filter(bidder=user)
 				serviceserializer = PublicServiceProviderBidSerializer(bidon, many=True)
 
-				services = services.exclude(bid=Bid.objects.filter(bidder=user))
+				services = services.exclude(bid__in=Bid.objects.filter(bidder=user))
 				serializer = PublicServiceSerializer(services, many=True)
 
 				data = {'feed':serializer.data}
